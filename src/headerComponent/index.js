5c7962f5c66cc6ba19570c49daf7ec0cfa54f965
poppymills2@gmail.com <poppymills2@gmail.com>
2021-11-30 22:48:17 -0500

    update nav

--- src/headerComponent/index.js
+++ src/headerComponent/index.js
@@ -10,57 +10,69 @@ import {
   Collapse,
   Button,
   Input,
+  CardImg,
 } from "reactstrap";
 import "./style.css";
 
 function Header() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
-  const toggle = () => setIsOpen(!isOpen);
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <div className="header">
-      {/* <Navbar expand="md" className="navbar_main"> */}
-      <div className="header_title">
-        <NavbarText className="logo">
-          Se<span>las</span>i K.
-        </NavbarText>
-        <NavbarText className="logo_title">DIRECTOR // DP // EDITOR</NavbarText>
-      </div>
-      <div className="header_below">
-        {/* <NavbarToggler className="toggle" onClick={toggle} /> */}
-        {/* <Input
+      <Navbar className="navbar_main">
+        <div className="header_title">
+          {/* <NavbarText className="logo"> */}
+          <img className="logo" src="images/1200.jpg" />
+          {/* Se<span>las</span>i K. */}
+          {/* </NavbarText> */}
+          {/* <NavbarText className="logo_title">
+            DIRECTOR // DP // EDITOR
+          </NavbarText> */}
+        </div>
+        <div className="header_below">
+          <a className="toggle" href="#" onClick={toggle}>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </a>
+
+          {/* <NavbarToggler className="toggle" onClick={toggle} /> */}
+          {/* <Input
             type="button"
             onClick={toggle}
             value="menu"
             className="toggle"
           /> */}
 
-        {/* <Collapse isOpen={isOpen} navbar> */}
-        <Nav className="mr-auto header_links">
-          <NavItem className="item_nav">
-            <Link to="/" className="item_link">
-              Commercial.
-            </Link>
-          </NavItem>
-          <NavItem className="item_nav">
-            <Link to="/creative" className="item_link">
-              Creative.
-            </Link>
-          </NavItem>
-          <NavItem className="item_nav">
-            <Link to="/contact" className="item_link">
-              Contact.
-            </Link>
-          </NavItem>
-          <NavItem className="item_nav">
-            <Link to="/about" className="item_link">
-              About.
-            </Link>
-          </NavItem>
-        </Nav>
-        {/* </Collapse> */}
-      </div>
-      {/* </Navbar> */}
+          <Collapse isOpen={isOpen}>
+            <Nav className="header_links">
+              <NavItem className="item_nav">
+                <Link to="/" className="item_link">
+                  Commercial.
+                </Link>
+              </NavItem>
+              <NavItem className="item_nav">
+                <Link to="/creative" className="item_link">
+                  Creative.
+                </Link>
+              </NavItem>
+              <NavItem className="item_nav">
+                <Link to="/contact" className="item_link">
+                  Contact.
+                </Link>
+              </NavItem>
+              <NavItem className="item_nav">
+                <Link to="/about" className="item_link">
+                  About.
+                </Link>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </div>
+      </Navbar>
     </div>
   );
 }
